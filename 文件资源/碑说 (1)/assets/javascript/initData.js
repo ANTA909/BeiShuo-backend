4d3b3e555ac7004ac15958b7c88c9b3fa7737440
MiyanShiho <liuminxuan175@outlook.com>
2025-11-17 18:23:53 +0800

    初始化仓库

---
+++ 文件资源/碑说 (1)/assets/javascript/initData.js
@@ -0,0 +1,216 @@
+const resolutionInfo = {
+    "web": [
+      {
+        "label": "网页 1920",
+        "value": "1920x1080",
+        "w": 1920,
+        "h": 1080
+      },
+      {
+        "label": "网页 1440",
+        "value": "1440x1024",
+        "w": 1440,
+        "h": 1024
+      },
+      {
+        "label": "MacBook Pro 16\"",
+        "value": "1728x1117",
+        "w": 1728,
+        "h": 1117
+      },
+      {
+        "label": "MacBook Pro 14\"",
+        "value": "1512x982",
+        "w": 1512,
+        "h": 982
+      },
+      {
+        "label": "MacBook Pro",
+        "value": "1440x900",
+        "w": 1440,
+        "h": 900
+      },
+      {
+        "label": "MacBook Air 13",
+        "value": "1280x832",
+        "w": 1280,
+        "h": 832
+      },
+      {
+        "label": "iMac",
+        "value": "2240x1260",
+        "w": 2240,
+        "h": 1260
+      },
+      {
+        "label": "华为 MateBook 16s",
+        "value": "1260x840",
+        "w": 1260,
+        "h": 840
+      },
+      {
+        "label": "华为 MateBook 14s",
+        "value": "1260x840-14",
+        "w": 1260,
+        "h": 840
+      }
+    ],
+    "app": [
+      {
+        "label": "iPhone 16 Pro Max",
+        "value": "440x956-16pm",
+        "w": 440,
+        "h": 956
+      },
+      {
+        "label": "iPhone 16 Pro",
+        "value": "402x874-16p",
+        "w": 402,
+        "h": 874
+      },
+      {
+        "label": "iPhone 16 Plus",
+        "value": "430x932-16+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 16",
+        "value": "393x852-16",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Pro Max",
+        "value": "430x932-15pm",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15 Pro",
+        "value": "393x852-15p",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Plus",
+        "value": "430x932-15+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15",
+        "value": "393x852-15",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 14 Plus",
+        "value": "428x926-14+",
+        "w": 428,
+        "h": 926
+      },
+      {
+        "label": "iPhone 14",
+        "value": "390x844-14",
+        "w": 390,
+        "h": 844
+      },
+      {
+        "label": "iPhone 13 mini",
+        "value": "375x812-13m",
+        "w": 375,
+        "h": 812
+      },
+      {
+        "label": "iPhone 11",
+        "value": "414x896-11",
+        "w": 414,
+        "h": 896
+      },
+      {
+        "label": "iPhone SE",
+        "value": "320x568-se",
+        "w": 320,
+        "h": 568
+      },
+      {
+        "label": "Mate 70",
+        "value": "405x896-m70",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 70 Pro",
+        "value": "438x944-m70p",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 70 Pro+",
+        "value": "438x944-m70p+",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 60",
+        "value": "405x896-m60",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 60 Pro",
+        "value": "420x907-m60p",
+        "w": 420,
+        "h": 907
+      },
+      {
+        "label": "Mate XT-单屏",
+        "value": "336x744-mxt-s",
+        "w": 336,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-双屏",
+        "value": "682x744-mxt-d",
+        "w": 682,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-三屏",
+        "value": "1061x744-mxt-t",
+        "w": 1061,
+        "h": 744
+      },
+      {
+        "label": "Mate X5-展开",
+        "value": "741x832-mx5-u",
+        "w": 741,
+        "h": 832
+      },
+      {
+        "label": "Mate X5-折叠",
+        "value": "360x835-mx5-f",
+        "w": 360,
+        "h": 835
+      },
+      {
+        "label": "小米 15",
+        "value": "400x890-mi15",
+        "w": 400,
+        "h": 890
+      },
+      {
+        "label": "Samsung Galaxy S23",
+        "value": "360x7-s23",
+        "w": 360,
+        "h": 780
+      }
+    ]
+  }
+  
+const treeArr = [{"appuuid":"1988264127688081408","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-11 23:14:42","createUser":"WX20251111OCMX","default":false,"folderFlag":false,"id":395459,"name":"首页","pageName":"首页","pageScene":"web","pageType":"0","pageUuid":"1988264127725830144","remarks":"","sort":11,"status":"0","updateTime":"2025-11-12 11:38:28","updateUser":"WX20251111OCMX","url":"./assets/page/1988264127725830144.html","uuid":"1988264127725830144"},{"appuuid":"1988264127688081408","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-12 11:07:40","createUser":"WX20251111OCMX","default":false,"folderFlag":false,"id":397369,"name":"历史记录页面","pageName":"历史记录页面","pageScene":"web","pageType":"0","pageUuid":"1988443551448432640","sort":15,"status":"0","updateTime":"2025-11-13 16:40:17","updateUser":"WX20251111OCMX","url":"./assets/page/1988443551448432640.html","uuid":"1988443551448432640"},{"appuuid":"1988264127688081408","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-12 21:11:14","createUser":"WX20251111OCMX","default":false,"folderFlag":false,"id":401367,"name":"碑文识别页面","pageName":"碑文识别页面","pageScene":"web","pageType":"0","pageUuid":"1988595444078346240","sort":17,"status":"0","updateTime":"2025-11-13 16:40:24","updateUser":"WX20251111OCMX","url":"./assets/page/1988595444078346240.html","uuid":"1988595444078346240"},{"appuuid":"1988264127688081408","buildType":0,"childrenList":[{"appuuid":"1988264127688081408","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-12 18:40:20","createUser":"WX20251111OCMX","default":false,"folderFlag":false,"id":400924,"name":"主页面","pageName":"主页面","pageScene":"web","pageType":"0","pageUuid":"1988557465465126912","parentUuid":"1988889388368527360","sort":5,"status":"0","updateTime":"2025-11-13 16:40:46","updateUser":"WX20251111OCMX","url":"./assets/page/1988557465465126912.html","uuid":"1988557465465126912"},{"appuuid":"1988264127688081408","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-13 14:44:13","createUser":"WX20251111OCMX","default":false,"folderFlag":false,"id":403897,"name":"文章页面","pageName":"文章页面","pageScene":"web","pageType":"0","pageUuid":"1988860433636786176","parentUuid":"1988889388368527360","sort":7,"status":"0","updateTime":"2025-11-13 16:41:03","updateUser":"WX20251111OCMX","url":"./assets/page/1988860433636786176.html","uuid":"1988860433636786176"},{"appuuid":"1988264127688081408","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-13 15:49:30","createUser":"WX20251111OCMX","default":false,"folderFlag":false,"id":404439,"name":"我的收藏页面","pageName":"我的收藏页面","pageScene":"web","pageType":"0","pageUuid":"1988876862763302912","parentUuid":"1988889388368527360","sort":9,"status":"0","updateTime":"2025-11-13 16:41:11","updateUser":"WX20251111OCMX","url":"./assets/page/1988876862763302912.html","uuid":"1988876862763302912"}],"createTime":"2025-11-13 16:39:16","default":false,"folderFlag":true,"id":16419,"name":"碑文知识库页面","pageName":"碑文知识库页面","sort":18,"uuid":"1988889388368527360","version":0}]
+
+const projectName = '碑说'
+
+const appDoc = {"1988443551448432640":"\n"}
