c71652ab08bf33b0ab83f65b2b5e147878df5b40
朱安琪 <2773692572@qq.com>
2025-11-19 21:00:28 +0800

    初始提交：后端框架配置

---
+++ src/main/java/com/beishuo/beishuoApplication.java
@@ -0,0 +1,11 @@
+package com.beishuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class beishuoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(beishuoApplication.class, args);
+    }
+}
